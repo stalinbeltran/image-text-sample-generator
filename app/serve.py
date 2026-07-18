7786abdc18c50131ccd4a745134cf093b3933490
Stalin Beltran <stalin.beltran2006@gmail.com>
2026-07-18 15:15:26 -0500

    puerto 8001

--- app/serve.py
+++ app/serve.py
@@ -38,7 +38,7 @@ class ProactorConfig(uvicorn.Config):
 def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--host", default="127.0.0.1")
-    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--port", type=int, default=8001)
     parser.add_argument("--reload", action="store_true")
     parser.add_argument("--log-level", default="info")
     args = parser.parse_args()
